40bb0584357506075f387b28b91151cfd7d268c2
Lexoner <Lexoner@Lexoner-Notebok>
2018-10-23 12:22:01 +0300

    tester creater

---
+++ Lesson_CVS_Egit/src/by/LCN/test/Main.java
@@ -0,0 +1,10 @@
+package by.LCN.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Git Hub!!!");
+	}
+
+}
